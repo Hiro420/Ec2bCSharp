a5158ebb5f8dbd1dd0d7b1dd8fb11b14ce3a8265
Hiro <84511935+Hiro420@users.noreply.github.com>
2025-09-08 02:36:03 +0300

    Initial commit

---
+++ Ec2bCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Ec2bKeyDeriver;
+
+internal static class Program
+{
+	static void Main(string[] args)
+	{
+		if (args.Length < 1)
+		{
+			Console.WriteLine("Usage: Ec2bKeyDeriver <ec2b.bin> [out.bin]");
+			return;
+		}
+
+		var ec2b = File.ReadAllBytes(args[0]);
+		var xorpad = Ec2b.Derive(ec2b); // 4096 bytes
+
+		var outPath = args.Length > 1 ? args[1] : "derived.bin";
+		File.WriteAllBytes(outPath, xorpad);
+
+		Console.WriteLine($"Derived xorpad: {xorpad.Length} bytes -> {outPath}");
+	}
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Ec2bCSharp
+C# fork of Mero's ec2b.py
